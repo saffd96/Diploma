03c6ffa1dbec7efe73f8e57fdd11345c7cb49eca
Saffd <saffd@tut.by>
2021-08-13 03:58:48 +0300

    added player animations for 3 player sprites

---
+++ Assets/Scripts/AnimationNames.cs
@@ -0,0 +1,17 @@
+public class AnimationTriggerNames
+{
+    public const string Jump = "Jump";
+    public const string GetDamage = "GetDamage";
+}
+
+public class AnimationFloatNames
+{
+    public const string Speed = "Speed";
+}
+
+public class AnimationBoolNames
+{
+    public const string IsRunning = "IsRunning";
+    public const string IsGrounded = "IsGrounded";
+    public const string IsDead = "IsDead";
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,13 +1,16 @@
-using System;
+using Unity.Mathematics;
 using UnityEngine;
 
+[RequireComponent(typeof(AnimationController))]
 public class PlayerMovement : MonoBehaviour
 {
     [Header("Move Settings")]
     [SerializeField] private float maxSpeed;
     [SerializeField] private bool isRunActive;
-    [SerializeField] private float speedMultiplier;
-    [SerializeField] private Transform groundChecker;
+    [SerializeField] private float runningSpeedMultiplier;
+    [SerializeField] private GameObject runVfx;
+    [Space]
+    [SerializeField] private Transform feetsPostion;
     [SerializeField] private float groundDetectRadius;
     [SerializeField] private LayerMask whatIsGround;
 
@@ -15,16 +18,28 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float jumpForce;
     [SerializeField] private bool isMultipleJumpsActive;
     [SerializeField] private int extraJumps;
+    [SerializeField] private GameObject extraJumpVfx;
+    [SerializeField] private float extraJumpVfxLifeTime;
 
-    [SerializeField] private float speed;
+    private GameObject dustFromRun;
+    private GameObject jumpVfx;
+    private AnimationController animationController;
+    private float speed;
     private int jumps;
     private bool isGrounded;
     private bool isFacingRight;
     private Rigidbody2D rb;
     private float moveInput;
 
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(feetsPostion.position, groundDetectRadius);
+    }
+
     private void Awake()
     {
+        animationController = GetComponent<AnimationController>();
         rb = GetComponent<Rigidbody2D>();
         isFacingRight = true;
         isGrounded = false;
@@ -39,54 +54,19 @@ public class PlayerMovement : MonoBehaviour
 
     private void Update()
     {
-        Jump();
-        Run();
-    }
-
-    private void Run()
-    {
-        switch (isRunActive)
-        {
-            case true when Input.GetKeyDown(KeyCode.LeftShift):
-                speed *= speedMultiplier;
-
-                break;
-            case true when Input.GetKeyUp(KeyCode.LeftShift):
-                speed /= speedMultiplier;
-
-                break;
-        }
-    }
-
-    private void Jump()
-    {
-        if (!Input.GetKeyDown(KeyCode.Space)) return;
-
-        if (isGrounded && jumps != extraJumps)
-        {
-            jumps = extraJumps;
-        }
-
-        if (jumps > 0 && isMultipleJumpsActive)
-        {
-            CalculateJumpVelocity();
-            jumps--;
-        }
-        else if (isGrounded && !isMultipleJumpsActive)
-        {
-            CalculateJumpVelocity();
-        }
-    }
-
-    private void CalculateJumpVelocity()
-    {
-        rb.velocity = Vector2.up * jumpForce;
+        CheckJumpCondition();
+        CheckRunCondition();
     }
 
     private void Move()
     {
-        isGrounded = Physics2D.OverlapCircle(groundChecker.position, groundDetectRadius, whatIsGround);
+        isGrounded = Physics2D.OverlapCircle(feetsPostion.position, groundDetectRadius, whatIsGround);
+        animationController.SetBool(AnimationBoolNames.IsGrounded, isGrounded);
+
         moveInput = Input.GetAxis("Horizontal");
+
+        animationController.SetFloat(AnimationFloatNames.Speed, Mathf.Abs(moveInput));
+
         rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
 
         if (moveInput > 0 && !isFacingRight)
@@ -107,5 +87,70 @@ public class PlayerMovement : MonoBehaviour
         isFacingRight = !isFacingRight;
         newScale.x *= -1;
         scaleTransform.localScale = newScale;
+    }
+
+    private void Jump()
+    {
+        animationController.SetTrigger(AnimationTriggerNames.Jump);
+        rb.velocity = Vector2.up * jumpForce;
+    }
+
+    private void CheckRunCondition()
+    {
+        SetActiveDustFromRun();
+
+        switch (isRunActive)
+        {
+            case true when Input.GetKeyDown(KeyCode.LeftShift):
+                dustFromRun = Instantiate(runVfx, transform);
+                dustFromRun.SetActive(false);
+                animationController.SetBool(AnimationBoolNames.IsRunning, true);
+                speed *= runningSpeedMultiplier;
+
+                break;
+            case true when Input.GetKeyUp(KeyCode.LeftShift):
+                Destroy(dustFromRun);
+                animationController.SetBool(AnimationBoolNames.IsRunning, false);
+                speed /= runningSpeedMultiplier;
+
+                break;
+        }
+    }
+
+    private void CheckJumpCondition()
+    {
+        if (!Input.GetKeyDown(KeyCode.Space)) return;
+
+        if (isGrounded)
+        {
+            jumps = extraJumps;
+        }
+        else
+        {
+            jumpVfx = Instantiate(extraJumpVfx, transform.position, quaternion.identity);
+            Destroy(jumpVfx, extraJumpVfxLifeTime);
+        }
+
+        switch (isMultipleJumpsActive)
+        {
+            case false:
+                Jump();
+
+                break;
+            case true when jumps >= 0:
+                Jump();
+                jumps--;
+
+                break;
+        }
+    }
+
+    private void SetActiveDustFromRun()
+    {
+        if (dustFromRun != null)
+        {
+            dustFromRun.SetActive(animationController.GetFloat(AnimationFloatNames.Speed) > 0.5f
+                && animationController.GetBool(AnimationBoolNames.IsGrounded));
+        }
     }
 }
